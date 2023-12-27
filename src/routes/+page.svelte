2a6e48de71e55214b6c5960dde2c9b3a740b46ec
Weiran Ye <williamwry@gmail.com>
2023-12-27 12:30:44 -0800

    Add search bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Heading, P, Button, ButtonGroup, Input } from 'flowbite-svelte';
+</script>
+
+<div class="text-center">
+	<Heading tag="h1">Wayfair Deep Search</Heading>
+	<Heading tag="h5">Help users go deeper into their search intent</Heading>
+</div>
+
+<div class="pt-8">
+	<ButtonGroup class="w-full">
+		<Input id="searchTerm" placeholder="what you are looking for? for example, sofa" />
+		<Button color="primary">Search</Button>
+	</ButtonGroup>
+</div>
